497d4f275478b3f460bf7b617d160ff30ee6e1fe
vincentchen743 <53363914+vincentchen743@users.noreply.github.com>
2021-12-19 20:20:25 +1100

    first commit

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,8 +12,8 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Vincent Chen</h2>
+        <p><a href="mailto:vincent.chen743@gmail.com">vincent.chen743@gmail.com</a></p>
       </header>
     </section>
 
