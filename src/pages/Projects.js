17ea92604ad944f5881df25b29e092b71bb341e9
vincentchen743 <53363914+vincentchen743@users.noreply.github.com>
2021-12-25 20:53:14 +1100

    first project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,13 +9,13 @@ import data from '../data/projects';
 const Projects = () => (
   <Main
     title="Projects"
-    description="Learn about Michael D'Angelo's projects."
+    description="Learn about Vincent's projects."
   >
     <article className="post" id="projects">
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/projects">Projects</Link></h2>
-          <p>TBC</p>
+          <p>Some projects that I've created!</p>
         </div>
       </header>
       {data.map((project) => (
